7d9fe404026ac988205d28170f8ca8a9e0eed964
Tudor <112909151+tudor-seserman@users.noreply.github.com>
2023-07-08 11:33:47 -0500

    week2 done

---
+++ Week2Challange.js
@@ -0,0 +1,9 @@
+function FirstReverse(str) { 
+    str = str.split('').reverse().join('');
+    // code goes here  
+    return str; 
+  
+  }
+     
+  // keep this function call here 
+  console.log(FirstReverse(readline()));
